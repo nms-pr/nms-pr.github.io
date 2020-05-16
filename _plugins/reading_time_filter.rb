131fbd630e4c6305a8e3c7cfb299049c84ec1e63
Mikhail Nechaev <nechaev.m.s@yandex.ru>
2020-05-16 19:22:34 +0700

    fix reading time

--- _plugins/reading_time_filter.rb
+++ _plugins/reading_time_filter.rb
@@ -4,34 +4,43 @@ module ReadingTimeFilter
   def reading_time(input)
     # Get words count.
     total_words = get_plain_text(input).split.size
-
-    # Load configuration.
-    config = @context.registers[:site].config["reading_time"]
-
-    # Setup default value.
-    if ! config
-      title = "Reading time"
-      less_then_minute = "less then minute"
-      minute_singular = "minute"
-      minute_plural = "minutes"
-    else
-      title = config["title"] ? config["title"] : "Reading time"
-      less_then_minute = config["less_then_minute"] ? config["less_then_minute"] : "less then minute"
-      minute_singular = config["minute_singular"] ? config["minute_singular"] : "minute"
-      minute_plural = config["minute_plural"] ? config["minute_plural"] : "minutes"
-    end
-
     # Average reading words per minute.
     words_per_minute = 180
-
     # Calculate reading time.
     case total_words
     when 0 .. 89
-      return "#{title}: #{less_then_minute}"
+      minutes = 0
     when 90 .. 269
+      minutes = 1
+    else
+      minutes = (total_words / words_per_minute ).floor
+    end
+    return convert_reading_min(minutes)
+  end
+
+  def convert_reading_min(minutes)
+    title = "Время чтения"
+
+    if minutes < 1
+      return "#{title}: меньше минуты"
+    end
+
+    n1 = minutes % 100
+    n2 = minutes % 10
+
+    if (n1 >= 11 and n1 <= 19) or (n2 >= 5) or (n2 == 0)
+      return "#{title}: #{minutes} минут";
+    elsif n2 == 1
+      return "#{title}: #{minutes} минута";
+    else
+      return "#{title}: #{minutes} минуты";
+    end
+
+    if minutes < 1
+
+    elsif minutes == 1
       return "#{title}: 1 #{minute_singular}"
     else
-      minutes = ( total_words / words_per_minute ).floor
       return "#{title}: #{minutes} #{minute_plural}";
     end
   end
